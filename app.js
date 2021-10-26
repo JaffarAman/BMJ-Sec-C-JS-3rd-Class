600c8300fbc32680eccc6585c45086519e0b871d
jaffar aman <amanjaffar50@gmail.com>
2021-10-26 12:37:36 +0500

    26/10/21 Js class

---
+++ app.js
@@ -0,0 +1,226 @@
+// alert("HELLO CLASS" + 100)
+// alert("10" + "10");
+
+
+// var sub1 = +prompt("Urdu mark")
+// var sub2 = +prompt("JS mark")
+// var sub3 = +prompt("HTML mark")
+
+
+// var totolMarks = sub1 + sub2 + sub3
+// var per = (totolMarks / 300) * 100
+// document.write("TOTOL MARKS:"   + totolMarks)
+// document.write("<br/> Percentage: "  + per)
+
+
+
+// var userAge = 22
+
+// if(userAge == 22){
+//     alert("HELLO USER")
+// }
+// else{
+//     alert("your age  incorrect")
+// }
+
+
+
+// var age = "20";
+
+
+// if(age == 20){
+//     document.write("your age is equal")
+// }else{
+//     document.write("your age is not equal")
+// }
+
+
+// var age = 14;
+// var age = +prompt("Enter age")
+
+// if(age == 14){
+//         document.write("Your age is equal")
+// }
+// else if(age > 14){
+//     alert("Your age is more")
+
+// }   
+// else if(age < 14){
+//     alert("your age is less")
+// }
+// else{
+//     alert("enter correct age")
+// }
+
+// var age = 22;
+
+// var age = prompt("age")
+
+// if(age != 20){
+//     alert("ALLOW")
+// }else{
+//     alert("NOT ALLOW")
+// }
+
+
+
+
+
+// var number = 20;
+
+
+
+// if(number == 20){
+//     alert("if condition")
+// }
+// else{
+//     alert("else condition")
+// }
+
+
+// var age = "20"; ///string
+
+// if(age === 20) {
+//         alert("age is equal")
+// }
+
+// else{
+//     alert("age is not equal")
+
+// }
+
+
+
+// var userAge = +prompt("enter age")
+// if(userAge == 20){
+//         alert("your age is equal")
+// }
+// else if(userAge > 20){
+//     alert("your age is more")
+// }
+// else if(userAge < 20 ){
+//     alert("your age is less")
+
+// }
+// else{
+//     alert("enter correct age")
+// }
+
+
+
+// //LOGICAL OPT///
+// var userAge = +prompt("enter your age");
+// var userGender = prompt("enter your Gender");
+// var userName = prompt("enter your Gender");
+
+// if(userAge >= 14 && userGender == "male" && userName == "jaffar"){
+//         document.write("allow")
+// }
+// else{
+//     document.write("not allow")
+    
+// }
+
+
+
+
+    /////MARK SHEET////
+
+// var percentage = +prompt("Enter Percentage")
+
+
+// if(percentage >= 80 && percentage <= 100){
+//         alert("WOW A+ kya bt hai...")
+// }
+// else if(percentage >=70 && percentage <= 79 ){
+//     alert("WOW A ...")
+
+// }
+// else if(percentage >=60 && percentage <=69){
+//     alert("GOOD B")
+// }
+// else if(percentage >=50 && percentage <=59){
+//     alert("NOt bad c")
+// } 
+
+// else if(percentage >=40 && percentage <=49){
+//     alert("Mehnat karo D")
+// }
+// else if(percentage >=0 && percentage <=39){
+//     alert("ABo bula rahy hai ghr pr FAIL ")
+// }
+// else{
+//     alert("enter correct percentage")   
+// }
+
+
+
+
+// var age = +prompt("age")
+
+// var gender = prompt("gender")
+
+
+// if(  age >= 20 && ( gender == "male" || gender == "female"   )  ){
+//     alert("if condition")
+// }
+// else{
+//     alert("else condition")
+
+// }
+
+
+
+
+
+                ////calculator //// 
+
+var num1 = +prompt("Enter Number 1");                
+var opt = prompt("Enter opt");                
+var num2 = +prompt("Enter Number 2");                
+
+
+
+if(opt === "+"){
+    alert(num1 + num2)
+}
+else if(opt === "-"){
+    alert(   - num2)
+}
+else if(opt === "*"){
+    alert(num1 * num2)
+
+}
+else if(opt === '/'){
+    alert(num1 / num2)
+
+}
+else{
+    alert("enter correct opt")
+}
+
+
+
+
+    /////chapter 1////
+    ///ques 1 //////
+// alert("Hello")
+
+    ///ques 2 ///
+
+alert("ERROR \n happy")
+
+
+var userName;
+
+var fullName = "Jaffar Aman"
+alert(fullName)
+
+
+var pizza = "pizza \n Pizz \n pi"
+alert(pizza)
+
+
+if(userData <= 0.25){
+    alert(refill)
+}
